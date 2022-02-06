0ba5446561d3ae3f5c87fcf710189ea9e08975b1
Andronik Maria <andronikmar@yandex.ru>
2022-02-06 16:51:43 +0300

    dr

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,6 +1,5 @@
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-
 from users.models import Follow
 from users.serializers import SubscribeUserSerializer
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from recipes.pagination import PageSizeNumberPagination
 from recipes.serializers import FollowSerializer
 
